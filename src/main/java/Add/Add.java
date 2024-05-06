9a9ba5890a85e0d574cc06b5ca6b15116446a7ac
bhuvan <bhuvan@sapient.ai>
2024-05-06 20:40:05 +0530

    Tests from maven

---
+++ src/main/java/Add/Add.java
@@ -0,0 +1,26 @@
+package Add;
+
+public class Add {
+
+    public double add1(double a, double b) {
+        return a + b;
+    }
+
+    public double add2(double a, double b) {
+        return a * b;
+    }
+
+    public double add3(double a, double b) {
+        if(a > b)
+            return a + b;
+        else
+            return 0;
+    }
+
+    public double add4(double a, double b) {
+        if(a < b)
+            return a + b;
+        else
+            return 0;
+    }
+}
