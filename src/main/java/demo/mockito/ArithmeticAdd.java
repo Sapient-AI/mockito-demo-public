df06489a2ac8e7a3104ec2cc74a832b41bd94f69
bhuvan <bhuvan@sapient.ai>
2024-10-01 15:04:57 +0530

    test

--- src/main/java/demo/mockito/ArithmeticAdd.java
+++ src/main/java/demo/mockito/ArithmeticAdd.java
@@ -2,10 +2,6 @@ package demo.mockito;
 
 public class ArithmeticAdd {
 
-    public float add(float a, float b) {
-        return a + b;
-    }
-
     public float add2(float a) {
         return a + 2;
     }
